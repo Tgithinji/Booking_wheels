1491edcca7d6c6d5cf92527903d59819a2fcb22e
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-12-02 17:48:36 +0300

    Create database models

---
+++ app/models.py
@@ -0,0 +1,68 @@
+"""Database Models"""
+from app import db
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model):
+    """User database model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    bookings = db.relationship('Booking', backref='client', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Admin(db.Model):
+    """Admin database model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    cars = db.relationship('Car', backref='owner', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Admin {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+class Car(db.Model):
+    """Car database model"""
+    id = db.Column(db.Integer, primary_key=True)
+    make = db.Column(db.String(140), nullable=False)
+    model = db.Column(db.String(140), nullable=False)
+    year = db.Column(db.String(140), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    image_file = db.Column(db.String(20), nullable=False, default='')
+    bookings = db.relationship('Booking', backref='car', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Car {}>'.format(self.make)
+
+
+class Booking(db.Model):
+    """Bookings database model"""
+    id = db.Column(db.Integer, primary_key=True)
+    avalable = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'))
+
+    def __repr__(self):
+        return '<booking at {}>'.format(self.avalable)
