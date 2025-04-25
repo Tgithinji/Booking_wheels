b60b3eaaf0b6923cdca762bc27107da155cfa45a
Tgithinji <t.muraguri420@gmail.com>
2025-04-25 09:13:35 +0300

    Debugging sending of emails

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
 FLASK_APP=booking_wheels.py
-FLASK_DEBUG=1
--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['noreply@booking_wheels.com']
+    ADMINS = ['noreply@demo.com']
 
     POSTS_PER_PAGE = 10
